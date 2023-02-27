4385b7213540a519e58231f7712a909e52ad462d
Andres Melendes <andresruse18@gmail.com>
2023-02-26 21:56:07 -0400

    start gameplay consumer

--- gameplay/consumers.py
+++ gameplay/consumers.py
@@ -6,13 +6,18 @@ from channels.generic.websocket import WebsocketConsumer
 from print_pp.logging import Print
 
 
+from .phrases import generate_options
+
 class GamePlayConsumer(WebsocketConsumer):
-    def connect(self):
-        Print('connected')
+
+    def connect(self):    
+        self.current_phrases = generate_options()
         self.accept()
+
 
     def disconnect(self, close_code):
         pass
+
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
---
+++ gameplay/phrases.py
@@ -0,0 +1,37 @@
+import os
+
+
+import openai
+
+
+from gameplay.models.models import StoryTopic
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+
+def generate_options(self, options_to_generate:int, topic:StoryTopic) -> list[str]:
+    openai.api_key = OPENAI_API_KEY
+    current_options = []
+
+    if topic: 
+        prompt = f'Genera una frase aleatoria, de no mas de 5 palabras, con temática de {topic.spanish_name}, no te preocupes por la gramática, solo por la temática.'
+    else:
+        prompt = 'Genera una frase aleatoria, de no mas de 5 palabras, con temática aleatoria.'
+        
+    for _ in range(options_to_generate):
+        response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0.8, max_tokens=256)
+        current_options.append(response['choices'][0]['text'])
+    
+    return self.clean_options(current_options)
+
+
+def clean_options(self, options:list[str]) -> list[str]:
+    for i, option in enumerate(options):
+        option = option.replace('"', '').replace('\n', '')
+        options[i] = option
+    return options
