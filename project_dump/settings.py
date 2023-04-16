5f3ddca3451e55137de5a5d05d97c7ea9e254691
Andres Melendes <andresruse18@gmail.com>
2023-04-16 10:27:16 -0400

    add cors

--- project_dump/settings.py
+++ project_dump/settings.py
@@ -38,8 +38,9 @@ CSRF_TRUSTED_ORIGINS = ['https://projectdumpapi-production.up.railway.app']
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
-    "http://localhost:5173",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:5174",
+    "http://localhost:3000",
     "http://localhost:5174",
     "https://sprightly-parfait-220c08.netlify.app",
 ]
